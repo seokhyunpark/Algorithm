77dc1c027dfd28b4002f003c3bdc1aeb4c1ba917
박석현 <122147233+seokhyunpark@users.noreply.github.com>
2025-01-17 16:51:46 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -4,14 +4,14 @@ typedef long long ll;
 
 ll A, B, C;
 
-ll go(ll a, ll b) {
+ll go(ll b) {
     if (b == 1) {
-        return a % C;
+        return A % C;
     }
-    ll ret = go(a, b / 2);
+    ll ret = go(b / 2);
     ret = (ret * ret) % C;
     if (b % 2 == 1) {
-        ret = (ret * a) % C;
+        ret = (ret * A) % C;
     }
     return ret;
 }
@@ -20,7 +20,7 @@ int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
     cin >> A >> B >> C;
-    cout << go(A, B) << "\n";
+    cout << go(B) << "\n";
 
     return 0;
 }
